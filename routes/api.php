8bab2ad3b59047ebcbea7edc7d5eee16cde6a7f5
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-20 10:14:16 +0800

    messege

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use App\Http\Controllers\ApiProductController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/show',[ApiAccountManagerController::class, 'showproduct']);
+Route::get('/api/show',[ApiAccountManagerController::class, 'showproduct']);
