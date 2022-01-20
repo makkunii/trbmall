f376574decd0b16cac19d2ed9ed5de7c103ff5f8
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-20 19:10:58 +0800

    messege

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,16 +10,11 @@ class Product extends Model
     protected $table = 'products';
     protected $fillable = [
         'name',
-        'category_id',
-        'tax_id',
-        'generic_name',
-        'drug_class',
         'description',
         'price',
-        'measurement',
-        'is_prescription',
-        'is_available',
-        'is_active',
-        'image'
-    ];
+        'category_id',
+        'weight',
+        'length',
+        'height',
+        'status' 
 }
