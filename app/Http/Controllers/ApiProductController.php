2b66994707104dcf59d838a739b23ac368f57cb4
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-20 10:33:38 +0800

    messege

--- app/Http/Controllers/ApiProductController.php
+++ app/Http/Controllers/ApiProductController.php
@@ -133,7 +133,7 @@ public function insertproduct(Request $request) {
           'is_available',
           'is_active',
           'image')
-          ->where('tbl_product',$id)
+          ->where('products',$id)
           ->first();
           return response()->json(['Edit' => $fetchedit], 200);
       }
