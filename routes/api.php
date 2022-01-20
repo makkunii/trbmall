eba79177137add85dc5e4b79da59a6d3d433604b
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-20 10:31:18 +0800

    messege

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/products',[ApiProductController::class, 'showproduct']);
-Route::post('/insertproducts',[ApiProductController::class, 'insertproduct']);
+Route::get('/editproduct',[ApiProductController::class, 'editproduct']);
