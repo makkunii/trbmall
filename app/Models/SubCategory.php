663a12b5ce7e8475c052aa8785d807440011c836
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-21 15:29:09 +0800

    messege

---
+++ app/Models/SubCategory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'is_active',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $attributes = [
+        'is_active' => 1,
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+}
