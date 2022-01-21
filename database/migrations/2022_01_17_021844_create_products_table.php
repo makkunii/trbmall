9a2257807434bfe7139b1b93caf3dc887145c254
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-21 13:33:19 +0800

    messege

--- database/migrations/2022_01_17_021844_create_products_table.php
+++ database/migrations/2022_01_17_021844_create_products_table.php
@@ -15,14 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('name')->nullable();
-            $table->foreignId('description')->nullable();
-            $table->foreignId('price')->nullable();
-            $table->foreignId('category_id')->nullable();
-            $table->foreignId('weight')->nullable();
-            $table->foreignId('length')->nullable();
-            $table->foreignId('height')->nullable();
-            $table->foreignId('status')->nullable();
+          
             $table->timestamps();
         });
     }
