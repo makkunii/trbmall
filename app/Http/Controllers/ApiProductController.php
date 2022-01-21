736f9d67797d96c7637b0da6d91447fff2aea1a6
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-21 14:54:48 +0800

    messege

---
+++ app/Http/Controllers/ApiProductController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use DB;
+class ApiProductController extends Controller
+{
+  //**************************INSERT**************************//
+public function insertproduct(Request $request) {
+       // VALIDATE PRODUCT
+       $request->validate([
+           'name' => 'required|string|max:255',
+           'description' => 'required',
+           'price' => 'float',
+           'category_id' => 'required',
+           'weight' => 'nullable',
+           'length' => 'nullable',
+           'height' => 'nullable',
+           'status' => 'required|string|max:255'
+       ]);
+
+       // CREATE PRODUCT
+       Product::create([
+        'name' => $request->input('name'),
+        'description' => $request->input('description'),
+        'price' => $request->input('price'),
+        'category_id' => $request->input('category_id'),
+        'weight' => $request->input('weight'),
+        'length' => $request->input('length'),
+        'height' => $request->input('height'),
+        'status' => $request->input('status')
+        
+
+       ]);
+
+       // REDIRECT TO PRODUCT INDEX
+       return redirect()->route('index')->with('message', $request->name . ' has been saved.');
+   }
+
+  //**************************UPDATE**************************//
+   public function updateproduct(Request $request, $id){
+          // VALIDATE PRODUCT
+          $request->validate([
+            'name' => 'required|string|max:255',
+           'description' => 'required',
+           'price' => 'float',
+           'category_id' => 'required',
+           'weight' => 'nullable',
+           'length' => 'nullable',
+           'height' => 'nullable',
+           'status' => 'required|string|max:255'
+        ]);
+
+          // UPDATE PRODUCT
+          Product::where('id', $id)
+          ->update([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+            'category_id' => $request->input('category_id'),
+            'weight' => $request->input('weight'),
+            'length' => $request->input('length'),
+            'height' => $request->input('height'),
+            'status' => $request->input('status')
+      ]);
+
+
+       // REDIRECT TO PRODUCT INDEX
+       return redirect()->route('index')->with('message', $request->name . ' has been updated.');
+   }
+
+
+//    public function disableproduct(Request $request){
+//        $request->validate([
+//            'is_active' => 'required'
+//        ]); // this is validation for api before update
+
+//        //update in database
+//        $update = DB::table('product')
+//        ->where('id', $request->id)
+//        ->update([
+//            'is_active' => $request->is_active
+//        ]);
+//        return response()->json(['Success' => 'Product Disable'],200);
+
+// }
+
+   //**************************SHOW VIEW**************************//
+   public function showproduct(){
+           $fetchedit = DB::table('products')
+           ->select( 'name',
+           'description', 
+           'price',
+           'category_id',
+           'weight',
+           'length',
+           'height',
+           'status')
+           ->get();
+           return response()->json(['Show' => $fetchedit], 200);
+       }
+
+  //**************************EDIT VIEW**************************//
+  public function editproduct($id){
+          $fetchedit = DB::table('products')
+          ->select( 'name',
+          'description',
+          'price',
+          'category_id',
+          'weight',
+          'length',
+          'height',
+          'status')
+          ->where('id',$id)
+          ->first();
+          return response()->json(['Edit' => $fetchedit], 200);
+      }
+}
