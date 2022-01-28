973c7696812de8de9399c9149cea9133b7dea493
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-01-28 16:55:00 +0800

    messege

--- app/Http/Controllers/ApiProductController.php
+++ app/Http/Controllers/ApiProductController.php
@@ -56,7 +56,8 @@ public function insertproduct(Request $request) {
         ]);
 
           // UPDATE PRODUCT
-          Product::where('id', $id)
+          $update = DB::table('products')
+            ->where('id', $request->id)
           ->update([
             'name' => $request->input('name'),
             'description' => $request->input('description'),
