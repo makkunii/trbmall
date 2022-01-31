09daebdef8ada82e7483653165fe36e6a6cff0b4
kidrv1 <kidrv1@gmail.com>
2022-01-31 19:59:29 +0800

    working category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,24 +1,48 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Product;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class HomeController extends Controller
-{
-    public function index()
-    {
-        $products = Product::all();
-
-        return view('layouts/products', compact('products'));
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-
-}
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+
+
+use App\Models\Product;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+
+
+class HomeController extends Controller
+
+{
+
+    public function index()
+
+    {
+
+        $products = Product::all();
+
+
+
+        return view('layouts/products', compact('products'));
+
+    }
+
+
+
+    /**
+
+     * Show the application dashboard.
+
+     *
+
+     * @return \Illuminate\Contracts\Support\Renderable
+
+     */
+
+
+
+}
+
