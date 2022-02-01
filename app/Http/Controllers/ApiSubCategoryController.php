6e7e5c26a6f9775e01c3daa9d89f3080b09587f1
John Wendell Calanoc <MBRSPRT101@GMAIL.COM>
2022-02-01 12:11:03 +0800

    messege

--- app/Http/Controllers/ApiSubCategoryController.php
+++ app/Http/Controllers/ApiSubCategoryController.php
@@ -13,19 +13,21 @@ public function insertsubcategory(Request $request) {
        // VALIDATE PRODUCT
        $request->validate([
            'name' => 'required|string|max:255',
+           'category_id' => 'required',
            'is_active' => 'required'
 
        ]);
 
        // CREATE PRODUCT
-       $insert = DB::table('category')
+       $insert = DB::table('sub_category')
        ->insertGetId([
            'name' => $request->name,
+           'category_id' => $request->category_id,
            'is_active' => $request->is_active
        ]);
 
        // REDIRECT TO PRODUCT INDEX
-       return response()->json(['Success' => 'Category Created'],200);
+       return response()->json(['Success' => 'SubCategory Created'],200);
    }
 
   //**************************UPDATE**************************//
@@ -33,32 +35,36 @@ public function insertsubcategory(Request $request) {
           // VALIDATE PRODUCT
           $request->validate([
             'name' => 'required|string|max:255',
+            'category_id' => 'required',
             'is_active' => 'required'
           ]);
 
           // UPDATE PRODUCT
-          $update = DB::table('category')
+          $update = DB::table('sub_category')
           ->where('id', $request->id)
         ->update([
             'name' => $request->name,
+            'category_id' => $request->category_id,
             'is_active' => $request->is_active
            ]);
 
        // REDIRECT TO PRODUCT INDEX
-       return response()->json(['Success' => 'Category Updated'],200);
+       return response()->json(['Success' => 'SubCategory Updated'],200);
    }
 
 
    public function disablesubcategory(Request $request){
        $request->validate([
-           'is_active' => 'required'
+            'category_id' => 'required',
+            'is_active' => 'required'
        ]); // this is validation for api before update
 
        //update in database
        $update = DB::table('sub_category')
        ->where('id', $request->id)
        ->update([
-           'is_active' => $request->is_active
+           'is_active' => $request->is_active,
+           'category_id' => $request->category_id
        ]);
        return response()->json(['Success' => 'SubCategory Disable'],200);
 
@@ -67,7 +73,7 @@ public function insertsubcategory(Request $request) {
    //**************************SHOW VIEW**************************//
    public function showsubcategory(){
            $fetchedit = DB::table('sub_category')
-           ->select('name','is_active')
+           ->select('name','category_id','is_active')
            ->get();
            return response()->json(['Show' => $fetchedit], 200);
        }
@@ -75,7 +81,7 @@ public function insertsubcategory(Request $request) {
   //**************************EDIT VIEW**************************//
   public function editsubcategory($id){
           $fetchedit = DB::table('sub_category')
-          ->select('name','is_active')
+          ->select('name','category_id','is_active')
           ->where('id',$id)
           ->first();
           return response()->json(['Edit' => $fetchedit], 200);
