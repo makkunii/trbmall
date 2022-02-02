03e247df050565749290b14fa4752a7b68dd1e95
makkunii <mace21218@gmail.com>
2022-02-02 17:04:24 +0800

    cart total compute

--- resources/views/layouts/cart.blade.php
+++ resources/views/layouts/cart.blade.php
@@ -31,12 +31,7 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="breadcrumb__text">
-                        @if ($message = Session::get('success'))
-                        <div class="p-4 mb-3 bg-green-400 rounded">
-                            <p class="text-green-800">{{ $message }}</p>
-                        </div>
-                    @endif
-                        <h2>Shopping Cart</h2>
+                        <h2 class="text-dark">Shopping Cart</h2>
                     </div>
                 </div>
             </div>
@@ -53,6 +48,7 @@
                         <table>
                             <thead>
                                 <tr>
+                                    <th></th>
                                     <th class="shoping__product">Products</th>
                                     <th>Price</th>
                                     <th>Quantity</th>
@@ -63,39 +59,39 @@
                             <tbody>
                                 @foreach ($cartItems as $item)
                                 <tr>
-                                    <td class="shoping__cart__item">
-                                        <img src="{{ asset('public/assets/img/cart/cart-1.jpg') }}" alt="">
-                                        <h5>{{ $item->name }}</h5>
-                                    </td>
-                                    <td class="shoping__cart__price">
-                                        P{{ $item->price }}
-                                    </td>
-                                    <td class="shoping__cart__quantity">
-                                        <div class="quantity">
-                                            <div>
-                                                <form action="{{ route('cart.update') }}" method="POST">
-                                                    @csrf
-                                                    <input type="hidden" name="id" value="{{ $item->id}}" >
-                                                  <input type="number" name="quantity" value="{{ $item->quantity }}"
-                                                  class="form-control" /><br>
-                                                  <button type="submit" class="btn btn-block btn-danger">update</button>
-                                                  </form>
-                                            </div>
+                                 <td>
+                                  <input type="checkbox" class="checkit">
+                                </td>
+                                <td class="shoping__cart__item">
+                                    <img src="{{ asset('public/assets/img/cart/cart-1.jpg') }}" alt="">
+                                    <h5>{{ $item->name }}</h5>
+                                </td>
+                                <td class="shoping__cart__price">
+                                    P{{ $item->price }}
+                                </td>
+                                <td class="shoping__cart__quantity">
+                                    <div class="quantity">
+                                        <div>
+                                            <form action="{{ route('cart.update') }}" method="POST">
+                                                @csrf
+                                                <input type="hidden" name="id" value="{{ $item->id}}" >
+                                              <input type="number" name="quantity" value="{{ $item->quantity }}"
+                                              class="form-control" /><br>
+                                              <button type="submit" class="btn btn-block btn-danger">update</button>
+                                              </form>
                                         </div>
-                                    </td>
-                                    <td class="shoping__cart__total">
-                                        P{{ ($item->quantity* $item->price)  }}
-                                    </td>
-                                    <td class="shoping__cart__item__close">
-                                        <form action="{{ route('cart.remove') }}" method="POST">
-                                            @csrf
-                                            <input type="hidden" value="{{ $item->id }}" name="id">
-                                            <button class="btn btn-block btn-danger">x</button>
-                                        </form>
-                                    </td>
+                                    </div>
+                                </td>
+                                <td class="shoping__cart__total Total">
+                                    P<div class="ItemTotal" style="float:right">{{ ($item->quantity* $item->price)  }}</div>
+                                </td>
+
+
                                 </tr>
+
+
                                 @endforeach
-                            </tbody>
+                                </tbody>
                         </table>
                     </div>
                 </div>
@@ -124,7 +120,7 @@
                         <h5>Cart Total</h5>
                         <ul>
 
-                            <li>Total <span>P{{ Cart::getTotal() }}</span></li>
+                            <li>Total <span>P<div style="float:right" id="TotalAmt"></div></span></li>
 
                         </ul>
                         <a href="#" class="primary-btn">PROCEED TO CHECKOUT</a>
@@ -132,6 +128,32 @@
                 </div>
             </div>
         </div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $('.checkit').change(function () {
+    calculateTotals();
+}).change();
+
+
+// Calculate the total amount from selected items only
+function calculateTotals() {
+    var Sum = 0;
+    // iterate through each td based on class and add the values
+    $(".Total").each(function () {
+        //Check if the checkbox is checked
+        if ($(this).closest('tr').find('.checkit').is(':checked')) {
+            var value = $('.ItemTotal', this).text();
+            // add only if the value is number
+            if (!isNaN(value) && value.length != 0) {
+                Sum += parseFloat(value);
+            }
+        }
+    });
+    $('#TotalAmt').text(Sum.toFixed(2));
+};
+
+            </script>
     </section>
     <!-- Shoping Cart Section End -->
     @endsection
