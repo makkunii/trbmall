f0b3daba8b3a5d13b9aab2f9a0d97efd32f0a882
makkunii <mace21218@gmail.com>
2022-02-12 10:14:25 +0800

    test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,18 +7,18 @@
 
 
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('/public/dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/dist/css/adminlte.min.css') }}">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('/public/plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- DataTables -->
-  <link rel="stylesheet" href="{{ asset('/public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('/public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('/public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
   <!-- Select -->
-  <link rel="stylesheet" href="/public/plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="/public/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <link rel="stylesheet" href="/plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 
 </head>
 <body>
@@ -36,26 +36,26 @@
 
 
 <!-- jQuery -->
-<script src="{{ asset('/public/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('/public/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- DataTables  & Plugins -->
-<script src="{{ asset('/public/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/jszip/jszip.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/pdfmake/pdfmake.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/pdfmake/vfs_fonts.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-<script src="{{ asset('/public/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
 <!-- Select -->
-<script src="/public/plugins/select2/js/select2.full.min.js"></script>
+<script src="/plugins/select2/js/select2.full.min.js"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('/public/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('/dist/js/adminlte.min.js') }}"></script>
 <!-- Page specific script -->
 <script>
 
